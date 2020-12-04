db9facf470618efe837fc2a8e841cae956aeec7b
Krishna Manchikanti <krishnamanchikanti@Krishnas-MacBook-Air.local>
2020-12-04 09:50:42 +0530

    creating Schema for signup

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+//Schema wt we needed
+const userSchema = new mongoose.Schema=({
+    //fields
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required:true
+    },
+    name :{
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+// telling mongoose that it is model
+const User = mongoose.model('User',userSchema);
+//exporting our usermodel
+module.exports = user;
