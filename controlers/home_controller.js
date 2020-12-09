b9801cc27eab2203115a73c6284b2a483b8d01a9
Krishna Manchikanti <krishnamanchikanti@Krishnas-MacBook-Air.local>
2020-12-09 16:13:39 +0530

    Displaying comments with nested population

--- controlers/home_controller.js
+++ controlers/home_controller.js
@@ -1,6 +1,6 @@
 const posts = require('../models/post');
 
-
+const comments = require('../models/comment');
 module.exports.home = (req,res)=>{
     // posts.find({},(err,posts)=>{
     //     if(err){
@@ -11,15 +11,35 @@ module.exports.home = (req,res)=>{
     //         posts:posts
     //     });
     // });
-    posts.find({}).populate('user').exec( (err,posts)=>{
+    posts.find({})
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    })
+    .exec( (err,posts)=>{
         if(err){
             console.log(`err in finding posts${err}`);
             return;
         }
         return res.render('home',{
-            posts:posts
+            posts:posts,
+            comments:'hello'
         }); 
     });
 };
 
-
+// module.exports.comments = (req,res)=>{
+//     comments.find({}).populate('user').populate('posts').exec((err,comments)=>{
+//         if(err){
+//             console.log(`err in finding posts${err}`);
+//             return;
+//         }
+//         console.log(comments);
+//         return res.render('home',{
+//             comments:comments
+//         }); 
+//     });
+// };
